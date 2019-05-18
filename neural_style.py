60b1767415cc417e076843cd5b95ebe066954bff
Samuel Hinton <samuelreay@gmail.com>
2019-05-18 15:48:13 +1000

    jpg output now

--- neural_style.py
+++ neural_style.py
@@ -625,9 +625,9 @@ def get_optimizer(loss):
 def get_output_name(iteration):
     style_name = "_".join([i.split(".")[0] for i in args.style_imgs])
     if args.optimizer == "adam":
-        name = f"{args.content_img.split('.')[0]}_{style_name}_{args.learning_rate}_{iteration}.png"
+        name = f"{args.content_img.split('.')[0]}_{style_name}_{args.learning_rate}_{iteration}.jpg"
     else:
-        name = f"{args.content_img.split('.')[0]}_{style_name}_{iteration:04d}.png"
+        name = f"{args.content_img.split('.')[0]}_{style_name}_{iteration:04d}.jpg"
     return name
 
 
